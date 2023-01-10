d6509415b87cc1e01d4b69ae6c156d91e70c9644
DDanDev <110492441+DDanDev@users.noreply.github.com>
2023-01-10 20:12:36 -0300

    added option to disable looping slides

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ let currentSlide = 0;
 function changeSlide(direction) {
 	direction = direction.target.id;
 
+    //comment or not the line below to enable or disable looping around at the ends of the slides
+    if ((direction === "forwardArrow" && currentSlide === slides.length - 1) || (direction === "backArrow" && currentSlide === 0)) return;
+
 	slides[currentSlide].classList.remove("view");
 	direction === "backArrow" ? (currentSlide === 0 ? (currentSlide = slides.length - 1) : --currentSlide) : currentSlide === slides.length - 1 ? (currentSlide = 0) : ++currentSlide;
 	slides[currentSlide].classList.add("view");
 
+    //comment or not the couple lines below to enable or disable greying out the arrows at the start or end of the slides
 	currentSlide === 0 ? backArrow.classList.add("disabledNav") : backArrow.classList.remove("disabledNav");
 	currentSlide === slides.length - 1 ? forwardArrow.classList.add("disabledNav") : forwardArrow.classList.remove("disabledNav");
 }
